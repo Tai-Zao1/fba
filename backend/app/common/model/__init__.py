022fee82d63f2289edc04b487c713817a1fe633c
孙志宇 <57326939@qq.com>
2025-02-20 17:19:28 +0800

    feat: 魔改代码，修改为多工厂模式

---
+++ backend/app/common/model/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .address import Address
+from .config import Config
+from .data_rule import DataRule
+from .dept import Dept
+from .dict_data import DictData
+from .dict_type import DictType
+from .login_log import LoginLog
+from .menu import Menu
+from .opera_log import OperaLog
+from .role import Role
+from .store import Store
+from .user import User
+from .user_social import UserSocial
+
+__all__ = [
+    "Address",
+    "Config",
+    "DataRule",
+    "Dept",
+    "DictData",
+    "DictType",
+    "LoginLog",
+    "Menu",
+    "OperaLog",
+    "Role",
+    "Store",
+    "User",
+    "UserSocial"
+]
